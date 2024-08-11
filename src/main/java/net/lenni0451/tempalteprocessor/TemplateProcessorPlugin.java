c71ca591dc38825876db286f4d4a9775792a10a0
Lenni0451 <20379977+Lenni0451@users.noreply.github.com>
2024-08-11 20:19:27 +0200

    Un-hardcode task names

--- src/main/java/net/lenni0451/tempalteprocessor/TemplateProcessorPlugin.java
+++ src/main/java/net/lenni0451/tempalteprocessor/TemplateProcessorPlugin.java
@@ -4,13 +4,17 @@ import net.lenni0451.tempalteprocessor.extensions.TemplateProcessorExtension;
 import net.lenni0451.tempalteprocessor.tasks.TemplateProcessorTask;
 import org.gradle.api.Plugin;
 import org.gradle.api.Project;
+import org.gradle.api.tasks.SourceSet;
+import org.gradle.api.tasks.SourceSetContainer;
 
 public class TemplateProcessorPlugin implements Plugin<Project> {
+
+    private static final String PROCESS_TEMPLATES_TASK_NAME = "processTemplates";
 
     @Override
     public void apply(Project target) {
         TemplateProcessorExtension extension = target.getExtensions().create("templateProcessor", TemplateProcessorExtension.class, target);
-        target.getTasks().register("processTemplates", TemplateProcessorTask.class, task -> {
+        target.getTasks().register(PROCESS_TEMPLATES_TASK_NAME, TemplateProcessorTask.class, task -> {
             task.getTemplateDir().set(extension.getTemplateDir());
             task.getOutputDir().set(extension.getOutputDir());
 
@@ -18,9 +22,10 @@ public class TemplateProcessorPlugin implements Plugin<Project> {
         });
 
         target.afterEvaluate(project -> {
-            project.getTasks().getByName("compileJava").dependsOn("processTemplates");
-            project.getTasks().getByName("compileTestJava").dependsOn("processTemplates");
-            project.getTasks().getByName("sourcesJar").dependsOn("processTemplates");
+            for (SourceSet sourceSet : project.getExtensions().getByType(SourceSetContainer.class)) {
+                project.getTasks().getByName(sourceSet.getCompileJavaTaskName()).dependsOn(PROCESS_TEMPLATES_TASK_NAME);
+                project.getTasks().getByName(sourceSet.getSourcesJarTaskName()).dependsOn(PROCESS_TEMPLATES_TASK_NAME);
+            }
         });
     }
 
